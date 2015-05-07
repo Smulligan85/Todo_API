8b3d2c1eb4f44d852f3757807513bfdc8064422c
Sean Mulligan <sean.mulligan85@gmail.com>
2015-05-06 22:16:01 -0700

    Adding user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  
+  resources :users do
+    resources :lists, except: [:index]
+  end
+
+  namespace :api, defaults: { format: :json } do
+     resources :users
+  end
+
+
   get 'user/new'
 
   root 'welcome#index'
--- db/schema.rb
+++ db/schema.rb
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20150505140731) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
+    t.string   "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
