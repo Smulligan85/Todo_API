45e4e75d41581cf2c83ec853ece26219d18e0fee
Sean Mulligan <sean.mulligan85@gmail.com>
2015-05-11 07:35:00 -0700

    Fixing vaidate typo

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,5 @@
 class Item < ActiveRecord::Base
   belongs_to :list
+  validates_inclusion_of :private, in: [true, false]  
+
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,6 @@
 class List < ActiveRecord::Base
 
   validates :title, presence: true
-  validates_inclusion_of :private, in: [true, false]  
   belongs_to :user
   has_many :items
 end
